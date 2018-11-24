980b42ffa00913751e3473148461f3129f1096e6
Greg Kubisa <gkubisa@gmail.com>
2018-11-24 00:28:34 +0000

    Require 100% code coverage in unit tests

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,13 @@ module.exports = {
     collectCoverageFrom: [
         'src/**/*.{ts,tsx,js,jsx}',
         '!src/**/*.test.{ts,tsx,js,jsx}'
-    ]
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100
+        }
+    }
 }
