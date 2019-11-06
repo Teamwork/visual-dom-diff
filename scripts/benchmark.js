a37afcb5c6180877d6dbcbf566ee61091814c953
Greg Kubisa <gkubisa@gmail.com>
2019-11-06 10:16:25 +0100

    Simplify benchmark and test with more data

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -1,50 +1,21 @@
-const benchmark_1 = require("benchmark");
-const diff_match_patch_1 = require("diff-match-patch");
-const jsdom_1 = require("jsdom");
-const diff_1 = require("../lib/diff");
-function htmlToFragment(html) {
-    const template = new jsdom_1.JSDOM('Hello').window.document.createElement('template');
-    template.innerHTML = html;
-    return template.content;
-}
-const suite = new benchmark_1.Suite();
-const sampleData = [
-    [
-        'same structure but different nodes',
-        htmlToFragment('Prefix <ul><li>Test</li></ul> Suffix'),
-        htmlToFragment('Prefix <ol><li>Test</li></ol> Suffix'),
-        undefined,
-    ],
-    [
-        'formatting added',
-        htmlToFragment('Prefix StrongEm Suffix'),
-        htmlToFragment('Prefix <strong>Strong</strong><em>Em</em> Suffix'),
-        undefined,
-    ],
-    [
-        'custom diffText option',
-        htmlToFragment('one two'),
-        htmlToFragment('one two three'),
-        {
-            diffText: (oldText, newText) => {
-                const diff = [];
-                if (oldText) {
-                    diff.push([diff_match_patch_1.DIFF_DELETE, oldText]);
-                }
-                if (newText) {
-                    diff.push([diff_match_patch_1.DIFF_INSERT, newText]);
-                }
-                return diff;
-            },
-        },
-    ]
-];
-sampleData.forEach(sample => {
-    suite.add(sample[0], () => {
-        diff_1.visualDomDiff(sample[1], sample[2], sample[3]);
-    });
-});
-suite.on('cycle', function (event) {
-    console.log(String(event.target));
-});
-suite.run({ 'async': true });
+const benchmark = require('benchmark')
+const fs = require('fs')
+const jsdom = require('jsdom')
+const path = require('path')
+const diff = require('../lib/diff')
+
+const oldPath = path.join(__dirname, 'benchmark', 'old.html')
+const newPath = path.join(__dirname, 'benchmark', 'new.html')
+const oldHtml = fs.readFileSync(oldPath, 'utf8')
+const newHtml = fs.readFileSync(newPath, 'utf8')
+const oldNode = new jsdom.JSDOM(oldHtml).window.document.body
+const newNode = new jsdom.JSDOM(newHtml).window.document.body
+
+const suite = new benchmark.Suite()
+suite.add('diff', () => {
+    diff.visualDomDiff(oldNode, newNode)
+})
+suite.on('cycle', function(event) {
+    console.log(String(event.target))
+})
+suite.run({ async: true })
